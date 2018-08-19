baf34a761444fb2eaf2c8445e229899556659021
Sanyam Jain <sanyam@patchus.in>
2018-08-19 19:14:16 +0530

    initialized project

---
+++ Pjsip/Pjsip/Pjsip.h
@@ -0,0 +1,19 @@
+//
+//  Pjsip.h
+//  Pjsip
+//
+//  Created by Sanyam Jain on 19/08/18.
+//  Copyright © 2018 Sanyam Jain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Pjsip.
+FOUNDATION_EXPORT double PjsipVersionNumber;
+
+//! Project version string for Pjsip.
+FOUNDATION_EXPORT const unsigned char PjsipVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Pjsip/PublicHeader.h>
+
+
---
+++ Pjsip/Pods/Target Support Files/Pods-Pjsip/Pods-Pjsip-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Pjsip : NSObject
+@end
+@implementation PodsDummy_Pods_Pjsip
+@end
